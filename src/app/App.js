b9398800d5e1bc4d061149e39b94aac238d01355
David De Anda <omarandstuff@gmail.com>
2018-02-05 08:14:08 -0600

    App configuration component

--- src/app/App.js
+++ src/app/App.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './redux/store'
+
+import Layout from './components/layouts/MainLayout'
+
+import Index    from './components/beers/Index'
+import New      from './components/beers/New'
+import NotFound from './components/beers/NotFound'
+
 import './App.css'
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
-      <div>
-        Base App
-      </div>
+      <Provider store={store}>
+        <Router>
+          <Layout>
+            <Switch>
+              <Route exact path='/' component={ Index } />
+              <Route exact path='/log_my_beer' component={ New } />
+              <Route component={ NotFound } />
+            </Switch>
+          </Layout>
+        </Router>
+      </Provider>
     )
   }
 }
-
-export default App
